4ea1c98da95d64a9729294c3d280c3f43fb55346
Ronald Mera <roncollege1026@gmail.com>
2021-11-18 10:46:33 -0500

    frontend register page complete

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -5,6 +5,8 @@ export default function Register() {
     <div className="register">
       <span className="registerTitle">Register</span>
       <form className="registerForm">
+        <label>Username</label>
+        <input className="registerInput" type="text" placeholder="Enter your username..." />
         <label>Email</label>
         <input className="registerInput" type="text" placeholder="Enter your email..." />
         <label>Password</label>
