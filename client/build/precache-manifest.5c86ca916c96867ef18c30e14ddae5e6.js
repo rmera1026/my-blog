eeb1adc8d02e7ee67f448ad71af113a0ccbac472
Ronald Mera <roncollege1026@gmail.com>
2022-01-22 08:03:00 -0500

    build

---
+++ client/build/precache-manifest.5c86ca916c96867ef18c30e14ddae5e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6e7cdb793cc26bfea5e03c5ef65989f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f304e0bcbe6d442f4a64",
+    "url": "/static/css/main.e7d0e14a.chunk.css"
+  },
+  {
+    "revision": "5f528592defcdd3538ec",
+    "url": "/static/js/2.0a766f79.chunk.js"
+  },
+  {
+    "revision": "4aa839f25784e6ec59906b932c7e8c9f",
+    "url": "/static/js/2.0a766f79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f304e0bcbe6d442f4a64",
+    "url": "/static/js/main.b443d8ac.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
