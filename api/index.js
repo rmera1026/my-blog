b806caa6b8eeaa4627ede536d51c44aca1d8e35d
Ronald Mera <roncollege1026@gmail.com>
2022-01-24 18:03:57 -0500

    make it better

--- api/index.js
+++ api/index.js
@@ -40,6 +40,7 @@ app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
 
-app.listen("5000" , () => {
+
+app.listen(process.env.PORT || 3000 , () => {
   console.log("Backend is working")
 })
