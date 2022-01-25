36c2405f33408aaf247619a283407c69d3ff9957
Ronald Mera <roncollege1026@gmail.com>
2022-01-25 18:43:03 -0500

    api

--- api/index.js
+++ api/index.js
@@ -44,3 +44,5 @@ app.use("/api/categories", categoryRoute);
 app.listen(process.env.PORT || 5000 , () => {
   console.log("Backend is working")
 })
+
+app.get('/', (req, res) => { res.send('Hello from Express!')})
--- client/src/pages/settings/Settings.jsx
+++ client/src/pages/settings/Settings.jsx
@@ -13,7 +13,7 @@ export default function Settings() {
   const [success, setSuccess] = useState(false);
 
   const { user, dispatch } = useContext(Context);
-  const PF = "https://ronald-blog.herokuapp.com/images/"
+  const PF = "http://localhost:5000/images/"
 
   const handleSubmit = async (e) => {
     e.preventDefault();
