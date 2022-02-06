74a0090e20a73d29cacdd4be7555a770809209b8
Ronald Mera <roncollege1026@gmail.com>
2022-02-06 12:21:49 -0500

    Fixed proxy

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import "./post.css";
 
 export default function Post({post}) {
-  const PF = "https://ronald-blog.herokuapp.com/images/";
+  const PF = "http://localhost:5000/images/";
 
   return (
     <div className="post">
